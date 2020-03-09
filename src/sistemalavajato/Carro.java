ea374a6029a0831b8a1ed6f9cfcf6e014282501d
Marcelo <marcelodosanjosc@gmail.com>
2020-03-09 08:25:06 -0400

    Meu primeiro commmit

---
+++ src/sistemalavajato/Carro.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemalavajato;
+
+/**
+ *
+ * @author marcelo
+ */
+public class Carro {
+    private int codigo;
+    private String modelo;
+    private String marca;
+    private String cor;
+    private int ano;
+    private int acessorio;
+    private Cliente dono;
+}
---
+++ src/sistemalavajato/Cliente.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemalavajato;
+
+/**
+ *
+ * @author marcelo
+ */
+public class Cliente extends Pessoa{
+    private String cpf;
+   
+   
+}
